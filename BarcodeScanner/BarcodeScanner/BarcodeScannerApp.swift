57d4226f1cde96a3ca4cf73fc0d71735d005dead
Matteo <116072651+rimaout@users.noreply.github.com>
2025-08-18 15:39:34 +0200

    BarcodeScanner V1, used:  - Camera access from SwiftUI using delegate protocol  - Basic error handling and alerts

---
+++ BarcodeScanner/BarcodeScanner/BarcodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeScannerApp.swift
+//  BarcodeScanner
+//
+//  Created by Matteo on 14/08/25.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            BarcodeScannerView()
+        }
+    }
+}
