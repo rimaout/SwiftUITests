ccb81fab5a5ec3fa6dc5412840533211ea933b74
Matteo <116072651+rimaout@users.noreply.github.com>
2025-08-28 23:09:10 +0200

    AppetizesApp: added FocusState to AccountView form

--- Apperizers/Apperizers/Screens/AccountView/AccountView.swift
+++ Apperizers/Apperizers/Screens/AccountView/AccountView.swift
@@ -10,6 +10,17 @@ import SwiftUI
 struct AccountView: View {
     @StateObject var viewModel = AccountViewModel()
     
+    @FocusState private var focusedTextField: FormTextField?
+    enum FormTextField {
+        case firstName, lastName, email
+    }
+    
+    // Manage keyboard focus between form fields using FocusState:
+    // - .focused binds a field to the FocusState enum value when the user taps the field.
+    // - .onSubmit defines what happens when the user taps the keyboard's submit/return key.
+    // - .submitLabel customizes the label shown on the keyboard's submit button.
+    // This lets the return key move to the next field (or dismiss the keyboard on the last field).
+    
     var body: some View {
         NavigationStack {
             Form {
@@ -17,15 +28,24 @@ struct AccountView: View {
                     TextField("First Name", text: $viewModel.user.firstName)
                         .textContentType(.name)
                         .textInputAutocapitalization(.words)
+                        .focused($focusedTextField, equals: .firstName)
+                        .onSubmit { focusedTextField = .lastName }   // move to Last Name
+                        .submitLabel(.next)
                     
                     TextField("Last Name", text: $viewModel.user.lastName)
                         .textContentType(.name)
                         .textInputAutocapitalization(.words)
+                        .focused($focusedTextField, equals: .lastName)
+                        .onSubmit { focusedTextField = .email }      // move to Email
+                        .submitLabel(.next)
                     
                     TextField("Email", text: $viewModel.user.email)
                         .textContentType(.emailAddress)
                         .keyboardType(.emailAddress)
                         .textInputAutocapitalization(.none)
+                        .focused($focusedTextField, equals: .email)
+                        .onSubmit { focusedTextField = nil }         // last field: dismiss keyboard
+                        .submitLabel(.continue)
                     
                     DatePicker("Birtday", selection: $viewModel.user.birthdate, displayedComponents: .date)
                     
@@ -39,8 +59,13 @@ struct AccountView: View {
                     Toggle("Celiac", isOn: $viewModel.user.celiac)
                 }.toggleStyle(SwitchToggleStyle(tint: .brandPrimary))
             }
-            .navigationTitle("😃 Account")
+            .toolbar {
+                ToolbarItemGroup(placement: .keyboard) {
+                    Button("Done") { focusedTextField = nil }
+                }
+            }
         }
+        .navigationTitle("😃 Account")
         .onAppear {
             viewModel.retirveUser()
         }
